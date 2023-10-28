9f3832cd91026327a1645d745183ad20a103ce43
Поташов Олег <potashov_2004@mail.ru>
2023-10-28 20:15:00 +0500

    the header and main block have been laid out

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import './index.html';
 import './index.scss';
 
+console.log(window.innerHeight)
+
 // Переменная для определения анимации, если анимация идет в данный момент то функция работать не будет
 let menuAnimation = false;
 
 document.querySelector('.menu__burger').addEventListener('click', () => {
 
     if(!menuAnimation) {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+          });
 
         menuAnimation = true;
         document.body.classList.toggle('open')
         document.querySelector('.header__menu').classList.toggle('open');
         document.querySelector('.central__line').classList.toggle('open');
+
 
         setTimeout(() => {
             menuAnimation = false
